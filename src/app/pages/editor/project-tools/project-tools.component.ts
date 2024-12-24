e0db4a0ecedfc03b6678c241b5343b8a0faa6cb2
Roberto Albanesi <beto0607@gmail.com>
2024-12-24 17:46:17 +0100

    Refactor icons to use IconComponent

--- src/app/pages/editor/project-tools/project-tools.component.ts
+++ src/app/pages/editor/project-tools/project-tools.component.ts
@@ -1,28 +1,18 @@
 import { Component, inject } from '@angular/core';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import {
-  faDownload,
-  faHome,
-  faSave,
-  faShare,
-} from '@fortawesome/free-solid-svg-icons';
+import { IconComponent } from '../../../components';
 import { ColorPickerComponent } from '../../../components/color-picker/color-picker.component';
 import { HexColor } from '../../../domain';
 import { WorkspaceStateService } from '../../../state/workspace';
 import { SaveButtonComponent } from './save-button/save-button.component';
 
 @Component({
-  imports: [FaIconComponent, ColorPickerComponent, SaveButtonComponent],
+  imports: [IconComponent, ColorPickerComponent, SaveButtonComponent],
   selector: 'app-project-tools',
   templateUrl: './project-tools.component.html',
   styleUrl: './project-tools.component.scss',
 })
 export class ProjectToolsComponent {
   private workspaceStateService = inject(WorkspaceStateService);
-  faSave = faSave;
-  faHome = faHome;
-  faShare = faShare;
-  faDownload = faDownload;
 
   currentBackgroundColor = this.workspaceStateService.backgroundColor;
 
