2cbb4bbbb5956b6a25193e79671265e19e8a0726
Roberto Albanesi <beto0607@gmail.com>
2024-12-26 01:24:38 +0100

    Add BrushSizeComponent

--- src/app/pages/editor/workspace/drawing-canvas/drawing-canvas-tools/brush-size/brush-size.component.ts
+++ src/app/pages/editor/workspace/drawing-canvas/drawing-canvas-tools/brush-size/brush-size.component.ts
@@ -1,22 +1,39 @@
-import { Component, computed, inject } from '@angular/core';
-import { SliderComponent } from '../../../../../../components';
+import {
+  Component,
+  computed,
+  ElementRef,
+  inject,
+  viewChild,
+} from '@angular/core';
+import { IconComponent, SliderComponent } from '../../../../../../components';
 import { DrawingCanvasService } from '../../drawing-canvas.service';
 
 @Component({
-  imports: [SliderComponent],
+  imports: [SliderComponent, IconComponent],
   selector: 'app-brush-size',
   templateUrl: './brush-size.component.html',
   styleUrl: './brush-size.component.scss',
 })
 export class BrushSizeComponent {
   private drawingCanvasService = inject(DrawingCanvasService);
+  private elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
+
+  private popover = viewChild.required<ElementRef<HTMLDivElement>>('popover');
 
   currentSize = this.drawingCanvasService.currentSize;
   isDisabled = computed(
-    () => this.drawingCanvasService.currentTool() !== 'pencil',
+    () => this.drawingCanvasService.currentTool() === 'fill',
   );
 
   onPencilSizeChanged(newValue: number): void {
     this.drawingCanvasService.setCurrentSize(newValue);
+  }
+
+  onOpenSliderClicked(): void {
+    const popover = this.popover().nativeElement;
+    popover.showPopover();
+    const { x, y } = this.elementRef.nativeElement.getBoundingClientRect();
+    popover.style.left = x + 30 + 'px';
+    popover.style.top = y - 5 + 'px';
   }
 }
